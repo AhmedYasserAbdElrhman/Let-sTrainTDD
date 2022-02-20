43cb1b193198a60c7ed766bfedc021b43f6a67df
Ahmed <biko.yasser@gmail.com>
2022-02-20 15:07:05 +0200

    Implementing Network Service Class (Inject URL Session - Return Response Model)

--- Let'sTrainTDD/Signup/Network/SignupWebService.swift
+++ Let'sTrainTDD/Signup/Network/SignupWebService.swift
@@ -9,10 +9,12 @@ import Foundation
 
 class SignupWebService {
     
+    private var urlSession: URLSession
     private var urlString: String
     
-    init(urlString: String) {
+    init(urlString: String, urlSession: URLSession = .shared) {
         self.urlString = urlString
+        self.urlSession = urlSession
     }
 
     func signup(body: SignupRequestBody, handler: @escaping (SignupResponseModel?, SignupErrors?) -> Void) {
@@ -25,5 +27,13 @@ class SignupWebService {
         let body = try? JSONEncoder().encode(body)
         request.httpBody = body
         
+        urlSession.dataTask(with: request) { data, response, error in
+            
+            if let data = data, let signupResponseModel = try? JSONDecoder().decode(SignupResponseModel.self, from: data ) {
+                handler(signupResponseModel, nil)
+            } else {
+                
+            }
+        }.resume()
     }
 }
