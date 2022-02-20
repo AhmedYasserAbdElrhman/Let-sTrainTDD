4f977cc837a738de3084ccc246c6f60b1c7a1f35
Ahmed <biko.yasser@gmail.com>
2022-02-20 12:10:38 +0200

    Implementing Network Service Class (Create Initial SignupWebService Class And Tests)

---
+++ Let'sTrainTDD/SignupWebService.swift
@@ -0,0 +1,12 @@
+//
+//  SignupWebService.swift
+//  Let'sTrainTDD
+//
+//  Created by Ahmed Yasser on 20/02/2022.
+//
+
+import Foundation
+
+class SignupWebService {
+    
+}
---
+++ Let'sTrainTDDTests/SignupWebServiceTests.swift
@@ -0,0 +1,28 @@
+//
+//  SignupWebServiceTests.swift
+//  Let'sTrainTDDTests
+//
+//  Created by Ahmed Yasser on 20/02/2022.
+//
+
+import XCTest
+@testable import Let_sTrainTDD
+
+class SignupWebServiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testSignupWebService_WhenGivenSuccessfullResponse_ReturnsSuccess() {
+        
+        // Arrange
+        let sut = SignupWebService()
+    }
+
+}
